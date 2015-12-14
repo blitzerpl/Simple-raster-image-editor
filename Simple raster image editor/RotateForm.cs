aead987f9dd8d5625234df269db38c7feaf6b9e8
plukaszuk <plukaszuk@gmail.com>
2015-12-14 22:04:48 +0100

    added image rotation

---
+++ Simple raster image editor/RotateForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simple_raster_image_editor
+{
+    public partial class RotateForm : Form
+    {
+        public double Angle { get; set; }
+        public DialogResult DialogResult { get; set; }
+
+        public RotateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            Angle = (double)numericUpDownAngle.Value;
+            DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
